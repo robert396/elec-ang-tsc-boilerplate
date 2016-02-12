5f818b760da9fc975dbdb42789d384ad9e64631b
Robert Cloutman <cloutman.robert@gmail.com>
2016-02-12 09:46:33 +0000

    Added gulp tasks to compile ts files, copy static files and also added npm script to build and run

---
+++ tasks/build.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var gulp = require("gulp");
+var tsc = require("gulp-typescript");
+var jetpack = require("fs-jetpack");
+var projectDir = jetpack;
+var appDir = projectDir.cwd("./app");
+var buildDir = projectDir.cwd("./build");
+
+gulp.task("clean:build", function () {
+    return buildDir.dir(".", { empty: true });
+});
+
+var compileTypescript = function () {
+    var project = tsc.createProject(appDir.path("tsconfig.json"));
+
+    return gulp.src("app/**/*.ts")
+        .pipe(tsc(project))
+        .pipe(gulp.dest(buildDir.path("")));
+};
+gulp.task("tsc:compile", ["clean:build"], compileTypescript);
+
+var copyStaticFiles = function () {
+  projectDir.copy("app", buildDir.path(), {
+     overwrite: true,
+     matching: [
+         "./renderer/**/*.html",
+     ] 
+  });
+};
+gulp.task("copy:static", ["clean:build"], copyStaticFiles);
+
+gulp.task("build:finalize", ["clean:build"], function () {
+   var manifest = appDir.read("package.json", "json");
+   buildDir.write("package.json", manifest); 
+});
+
+gulp.task("build", ["tsc:compile", "copy:static", "build:finalize"]);
